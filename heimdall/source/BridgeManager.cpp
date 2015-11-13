b1d29dc126e8860bed32fb4a98e5c6df4cfa0f70
Selby Kendrick <selby.kendrick@gmail.com>
2015-11-13 03:21:40 -0600

    Update BridgeManager.cpp

--- heimdall/source/BridgeManager.cpp
+++ heimdall/source/BridgeManager.cpp
@@ -926,7 +926,7 @@ int BridgeManager::ReceivePitFile(unsigned char **pitBuffer) const
 	for (unsigned int i = 0; i < transferCount; i++)
 	{
 		DumpPartPitFilePacket *requestPacket = new DumpPartPitFilePacket(i);
-		success = SendPacket(requestPacket);
+		success = SendPacket(requestPacket, kDefaultTimeoutSend, kEmptyTransferNone);
 		delete requestPacket;
 
 		if (!success)
@@ -940,7 +940,6 @@ int BridgeManager::ReceivePitFile(unsigned char **pitBuffer) const
 		
 		ReceiveFilePartPacket *receiveFilePartPacket = new ReceiveFilePartPacket();
 		success = ReceivePacket(receiveFilePartPacket, kDefaultTimeoutReceive, receiveEmptyTransferFlags);
-
 		if (!success)
 		{
 			Interface::PrintError("Failed to receive PIT file part #%d!\n", i);
